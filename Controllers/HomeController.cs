44c138cc888ebbe3c5ac073d3bda2c34feb57a9d
Ergün Karakuş <35894461+ekarakus@users.noreply.github.com>
2019-05-27 15:30:37 +0300

    ilk yükleme

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnetcoreIleVeriTasima.Models;
+
+namespace aspnetcoreIleVeriTasima.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public IActionResult form(string ad,int yas) {
+            ViewData["ad"] = ad;
+            ViewData["yas"] = yas;
+            ViewData["mesaj"] = "Gönderdiğiniz veriler elimize ulaştı eline koluna sağlık";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult form()
+        {
+            return View();
+        }
+
+        public IActionResult al()
+        {
+            return View();
+        }
+
+        public IActionResult gonder()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            
+            personel enes = new personel()
+            {
+                ad = "enes",
+                yas = 17
+            };
+
+            return View(enes);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["ad"] = "Ali";
+            ViewData["yas"] = 18;
+            ViewData["iyiBiriMi"] = true;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.ad = "Ali";
+            ViewBag.yas = 18;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/personel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcoreIleVeriTasima.Models
+{
+    public class personel
+    {
+        public string ad { get; set; }
+        public int yas { get; set; }
+    }
+}
